6da8b35c1570bdd9a4813b97c7dcb4d82191b005
Timothy Cherney <tjcherney@hotmail.com>
2024-08-11 19:10:31 -0700

    million base build done, need to figure out adding memory map for windows to do large files

--- build.zig
+++ build.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
+
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -22,6 +23,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const ztracy = b.dependency("ztracy", .{
+        .enable_ztracy = true,
+        .enable_fibers = true,
+    });
+    exe.root_module.addImport("ztracy", ztracy.module("root"));
+    exe.linkLibrary(ztracy.artifact("tracy"));
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
--- src/main.zig
+++ src/main.zig
@@ -1,120 +1,92 @@
 const std = @import("std");
+const ztracy = @import("ztracy");
 
-const Measurement = struct {
-    value: f32 = undefined,
-    station: std.ArrayList(u8) = undefined,
-    pub fn print(self: *Measurement) void {
-        std.debug.print("Station: {s}, Measurement: {d}\n", .{ self.station.items, self.value });
-    }
-    pub fn deinit(self: *Measurement) void {
-        self.station.deinit();
-    }
-};
-
-const Calculation = struct {
-    station: []const u8 = undefined,
-    mean: f32 = undefined,
-    min: f32 = undefined,
-    max: f32 = undefined,
-    num: u32 = undefined,
-    pub fn calc_mean(self: *Calculation) void {
-        self.mean /= @as(f32, @floatFromInt(self.num));
-    }
-    pub fn print(self: *Calculation) !void {
-        const stdout_file = std.io.getStdOut().writer();
-        var bw = std.io.bufferedWriter(stdout_file);
-        const stdout = bw.writer();
-        try stdout.print("{s}={d}/{d}/{d}:Total{d}", .{ self.station, self.min, self.mean, self.max, self.num });
-        try bw.flush();
-    }
-};
-
-pub fn read_measurements(file_name: []const u8, allocator: std.mem.Allocator) !std.ArrayList(Measurement) {
-    var measurements: std.ArrayList(Measurement) = std.ArrayList(Measurement).init(allocator);
-    const file = try std.fs.cwd().openFile(file_name, .{});
-    defer file.close();
-    const size_limit = std.math.maxInt(u32);
-    const buffer = try file.readToEndAlloc(allocator, size_limit);
-    defer allocator.free(buffer);
-    var lines: std.mem.SplitIterator(u8, std.mem.DelimiterType.any) = std.mem.splitAny(u8, buffer, "\n");
-    var line = lines.next();
-    while (line != null) : (line = lines.next()) {
-        var measurement_iter: std.mem.SplitIterator(u8, std.mem.DelimiterType.any) = std.mem.splitAny(u8, line.?, ";");
-        const station = measurement_iter.next() orelse break;
-        const value_str = measurement_iter.next() orelse break;
-        var station_list = std.ArrayList(u8).init(allocator);
-        _ = try station_list.writer().write(station);
-        try measurements.append(Measurement{ .station = station_list, .value = try std.fmt.parseFloat(f32, value_str[0 .. value_str.len - 1]) });
-    }
-    return measurements;
+var timer: std.time.Timer = undefined;
+pub fn timer_start() !void {
+    timer = try std.time.Timer.start();
 }
 
-pub fn collect_measurements(meansurements: []Measurement, allocator: std.mem.Allocator) !std.StringHashMap(Calculation) {
-    var calculations = std.StringHashMap(Calculation).init(allocator);
-    for (meansurements) |measurement| {
-        var mapped_calc = calculations.get(measurement.station.items) orelse Calculation{ .station = measurement.station.items, .min = measurement.value, .max = measurement.value, .mean = 0.0, .num = 0 };
-        mapped_calc.max = if (measurement.value > mapped_calc.max) measurement.value else mapped_calc.max;
-        mapped_calc.min = if (measurement.value < mapped_calc.min) measurement.value else mapped_calc.min;
-        mapped_calc.mean += measurement.value;
-        mapped_calc.num += 1;
-        try calculations.put(mapped_calc.station, mapped_calc);
-    }
-    return calculations;
+pub fn timer_end(msg: []const u8) !void {
+    std.debug.print("{d} s elapsed.{s}\n", .{ @as(f32, @floatFromInt(timer.read())) / 1000000000.0, msg });
+    timer.reset();
 }
 
-pub fn process_calculations(calculations: std.StringHashMap(Calculation), allocator: std.mem.Allocator) !std.ArrayList(Calculation) {
-    var proccessed_calculations = std.ArrayList(Calculation).init(allocator);
-    var key_iter = calculations.keyIterator();
-    var next_key = key_iter.next();
-    while (next_key != null) : (next_key = key_iter.next()) {
-        var mapped_calc = calculations.get(next_key.?.*);
-        mapped_calc.?.calc_mean();
-        try proccessed_calculations.append(mapped_calc.?);
+const Calculation = struct { sum: f32 = undefined, min: f32 = undefined, max: f32 = undefined, num: u32 = undefined };
+
+pub fn parse_and_collect_measurement(buffer: []u8, calculations: *std.StringHashMap(Calculation), lock: *std.Thread.Mutex) !void {
+    var start: usize = 0;
+    while (start < buffer.len) {
+        const end: usize = std.mem.indexOfScalarPos(u8, buffer, start, '\n') orelse buffer.len;
+        var line = buffer[start..end];
+        const line_split = std.mem.indexOfScalar(u8, line, ';') orelse continue;
+        const station_name = line[0..line_split];
+        const value = try std.fmt.parseFloat(f32, line[line_split + 1 .. line.len - 1]);
+        lock.lock();
+        const station = try calculations.getOrPut(station_name);
+        if (station.found_existing) {
+            station.value_ptr.*.max = if (value > station.value_ptr.*.max) value else station.value_ptr.*.max;
+            station.value_ptr.*.min = if (value < station.value_ptr.*.min) value else station.value_ptr.*.min;
+            station.value_ptr.*.sum += value;
+            station.value_ptr.*.num += 1;
+        } else {
+            station.value_ptr.* = Calculation{ .min = value, .max = value, .sum = 0.0, .num = 1 };
+        }
+        lock.unlock();
+        start = end + 1;
     }
-    return proccessed_calculations;
+}
+
+pub fn thread_run(buffer: []u8, calculations: *std.StringHashMap(Calculation), lock: *std.Thread.Mutex) !void {
+    try parse_and_collect_measurement(buffer, calculations, lock);
 }
 
 pub fn main() !void {
+    const tracy_zone = ztracy.ZoneNC(@src(), "Compute Magic", 0x00_ff_00_00);
+    defer tracy_zone.End();
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    try bw.flush(); // Don't forget to flush!
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    var measurements: std.ArrayList(Measurement) = try read_measurements("measurements.txt", allocator);
-    var calculations = try collect_measurements(measurements.items, allocator);
-    const processed_calculations = try process_calculations(calculations, allocator);
-    calculations.deinit();
+    try timer_start();
+    const file = try std.fs.cwd().openFile("measurements.txt", .{});
+    const size_limit = std.math.maxInt(usize);
+    const buffer = try file.readToEndAlloc(allocator, size_limit);
+
+    try timer_end(" For input");
+    var lock = std.Thread.Mutex{};
+    const NUM_THREADS = 8;
+    var calculations: std.StringHashMap(Calculation) = std.StringHashMap(Calculation).init(allocator);
+    var threads: []std.Thread = try allocator.alloc(std.Thread, NUM_THREADS);
+    const data_split = buffer.len / NUM_THREADS;
+    var start: usize = 0;
+    for (0..NUM_THREADS) |i| {
+        const start_end: usize = (i + 1) * data_split;
+        const end: usize = std.mem.indexOfScalarPos(u8, buffer, start_end, '\n') orelse buffer.len;
+        threads[i] = try std.Thread.spawn(.{}, thread_run, .{ buffer[start..end], &calculations, &lock });
+        start = end + 1;
+        if (start >= buffer.len) break;
+    }
+    for (threads) |thread| {
+        thread.join();
+    }
+    allocator.free(threads);
+
+    try timer_end(" For calculations");
     try stdout.print("{{", .{});
-    for (0..processed_calculations.items.len) |i| {
-        try processed_calculations.items[i].print();
-        try stdout.print(", ", .{});
-        try bw.flush();
+    var key_iter = calculations.keyIterator();
+    var next_key = key_iter.next();
+    while (next_key != null) : (next_key = key_iter.next()) {
+        const station = calculations.get(next_key.?.*);
+        try stdout.print("{s}={d}/{d}/{d}, ", .{ next_key.?.*, station.?.min, station.?.sum / @as(f32, @floatFromInt(station.?.num)), station.?.max });
     }
     try stdout.print("}}\n", .{});
     try bw.flush();
-    for (0..measurements.items.len) |i| {
-        //measurements.items[i].print();
-        measurements.items[i].deinit();
-    }
-    processed_calculations.deinit();
-
-    measurements.deinit();
+    try timer_end(" For output");
+    file.close();
+    calculations.deinit();
+    allocator.free(buffer);
     if (gpa.deinit() == .leak) {
         std.debug.print("Leaked!\n", .{});
     }
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    // Try passing `--fuzz` to `zig build` and see if it manages to fail this test case!
-    const input_bytes = std.testing.fuzzInput(.{});
-    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
 }
